ca6451bc8fb925d6fbfddb21faff0bedfe570def
Hyeon-mook Jerry Choi <chm073@gmail.com>
2016-02-29 12:13:27 +0900

    add Function

---
+++ chapter3/Function.hs
@@ -0,0 +1,8 @@
+square x = x * x
+
+signum x =
+    if x < 0
+      then -1
+      else if x > 0
+       then 1
+       else 0
---
+++ chapter3/Signum.hs
@@ -0,0 +1,8 @@
+module Main where
+       
+signum x =
+    if x < 0
+      then -1
+      else if x > 0
+       then 1
+       else 0
